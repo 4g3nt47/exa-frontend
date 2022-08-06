ce74a9909b3839895be01c27b1511218cf8add5e
Umar Abdul <aufaruq02@gmail.com>
2022-08-06 07:40:20 +0100

    Finished implementing event logs

--- src/components/admin/EventLogs.svelte
+++ src/components/admin/EventLogs.svelte
@@ -34,21 +34,23 @@
     {#if (logs.length === 0)}
       <ErrorMsg error="No event logs available at the moment!"/>
     {:else}
-      {#each logs as log}
-        {#if (log.logType === 0)}
-          <div class="font-mono bg-green-500 mt-2 py-1 pl-2 bg-opacity-20 text-black border-l-4 border-green-800">
-            {new Date(log.date).toLocaleString()}: {log.message}
-          </div>
-        {:else if (log.logType === 1)}
-          <div class="font-mono bg-yellow-500 mt-2 py-1 pl-2 bg-opacity-20 text-black border-l-4 border-yellow-800">
-            {new Date(log.date).toLocaleString()}: {log.message}
-          </div>
-        {:else}
-          <div class="font-mono bg-red-500 mt-2 py-1 pl-2 bg-opacity-20 text-black border-l-4 border-red-800">
-            {new Date(log.date).toLocaleString()}: {log.message}            
-          </div>
-        {/if}
-      {/each}
+      <div class="w-full bg-gray-300 p-3 shadow-md shadow-gray-600">        
+        {#each logs as log}
+          {#if (log.logType === 0)}
+            <div class="font-mono bg-green-500 mt-2 py-1 pl-2 bg-opacity-20 text-black border-l-4 border-green-800">
+              {new Date(log.date).toLocaleString()}: {log.message}
+            </div>
+          {:else if (log.logType === 1)}
+            <div class="font-mono bg-yellow-500 mt-2 py-1 pl-2 bg-opacity-20 text-black border-l-4 border-yellow-800">
+              {new Date(log.date).toLocaleString()}: {log.message}
+            </div>
+          {:else}
+            <div class="font-mono bg-red-500 mt-2 py-1 pl-2 bg-opacity-20 text-black border-l-4 border-red-800">
+              {new Date(log.date).toLocaleString()}: {log.message}            
+            </div>
+          {/if}
+        {/each}
+      </div>
     {/if}
     <ErrorMsg {error}/>
   {/if}
