ac2ef78b81c3a0d8e07aff762dcb03b61fe3b7bf
Umar Abdul <aufaruq02@gmail.com>
2022-08-07 18:52:13 +0100

    Updated footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <!-- 
   The footer.
  -->
-<footer class="text-center text-lg mt-10 border-t border-black border-dashed p-5 mx-5 opacity-80">
+<footer class="text-center text-lg mt-10 border-t border-black p-5 mx-5 opacity-80">
   <p>Exa &copy; 2022, all rights reserved!</p>
-  <p>By: Umar Abdul</p>
+  <p>By: <a class="text-green-800" href="https://github.com/4g3nt47">Umar Abdul</a></p>
 </footer>
